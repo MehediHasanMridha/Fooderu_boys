06d1c47a91bb588890fbe5c16803bdb60070e9fe
HIMU005 <hashanuzzaman99@gmail.com>
2025-01-30 16:59:11 +0600

    finished the task

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
+import icon from "./assets/quick-setup.png";
 function App() {
   return (
     <div className="h-screen">
-      <div className="w-[382px] mx-auto mt-[116px] border">
+      <div className="w-[382px] md:w-[480px] h-[403px] md:h-[420px] mx-auto mt-[116px] md:mt-[333px] lg:mt-[116px]">
         <img
-          className="h-[270px] w-[270px] border mx-auto"
-          src="./assets/quick-setup.jpg"
+          className="h-[270px] md:h-[280px] w-[270px] md:w-[280px] mx-auto"
+          src={icon}
           alt="quick-setup image"
         />
-        <h2 className="text-2xl font-medium text-center my-6">
+        <h2 className="text-2xl md:text-[28px] font-medium text-center my-6 text-[#585858]">
           Let’s start a quick setup tour
         </h2>
-        <button className="text-white bg-gradient-to-r from-[#FFB800] to-[#FF7A00] w-full rounded-4xl text-lg font-medium leading-[30px] p-2.5">
+        <button className="text-white bg-gradient-to-r from-[#FFB800] to-[#FF7A00] w-full rounded-4xl text-lg md:text-xl font-medium leading-[30px] py-3">
           Start
         </button>
       </div>
