03585bd754e47412e83bc0156cfa646ab339decb
HIMU005 <hashanuzzaman99@gmail.com>
2025-02-03 20:14:49 +0600

    make it responsive for all devices

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import Step from "./Step";
 
 function App() {
   return (
-    <div className="w-[382px] mx-auto mt-[132px]">
+    <div className="w-[382px] md:w-[660px] lg:w-[942px] mx-auto mt-[132px] md:mt-[189px] lg:mt-[144px]">
       <Step />
       {/* input field and button */}
-      <div className="mt-[173px]">
-        <h1 className="text-lg font-medium leading-7">Let us know your name</h1>
+      <div className="mt-[173px] md:mt-[215px] lg:mt-[244px] mx-auto w-[382px] md:w-[452px] lg:w-[480px]">
+        <h1 className="text-lg md:text-[18.8px] lg:text-xl font-medium leading-7">
+          Let us know your name
+        </h1>
         <input
-          className="border-[#D2D2D2] border-2 rounded-[100px] w-[382px] h-[56px] px-4 my-6"
+          className="border-[#D2D2D2] border-2 rounded-[100px] w-full h-[56px] px-4 my-6 md:my-[22.6px]"
           type="text"
           placeholder="+880"
         />
-        <button className="rounded-[100px] w-[382px] h-[56px] bg-gradient-to-r from-[#FFB800] to-[#FF7A00] text-white">
+        <button className="rounded-[100px] w-full h-[56px] bg-gradient-to-r from-[#FFB800] to-[#FF7A00] text-white">
           Send OTP
         </button>
       </div>
--- src/Step.jsx
+++ src/Step.jsx
@@ -2,13 +2,13 @@ const Step = () => {
   const steps = [1, 2, 3, 4, 5, 6];
   const currentStep = 3; // Example: Step 3 is active
   return (
-    <div className="flex items-center justify-center space-x-0 p-4 rounded-md">
+    <div className=" md:w-[660px] lg:w-[942px] flex items-center justify-center space-x-0 p-4 rounded-md">
       {steps.map((step, index) => (
         <div key={step} className="flex items-center gap-0 text-sm">
           {/* Line before each step except the first */}
           {index !== 0 && (
             <div
-              className={`h-1 w-[47.6px] ${
+              className={`h-1 w-[47.6px] md:w-[95.8px] lg:w-[150px] ${
                 step <= currentStep ? "bg-orange-500" : "bg-gray-300"
               }`}
             ></div>
@@ -16,7 +16,7 @@ const Step = () => {
 
           {/* Step circle */}
           <div
-            className={`w-6 h-6 flex items-center justify-center rounded-full border ${
+            className={`w-6 md:w-[30.16px] h-6 md:h-[30.16px] flex items-center justify-center rounded-full border ${
               step < currentStep
                 ? " bg-gradient-to-r from-[#FFB800] to-[#FF7A00] text-white border-none"
                 : step === currentStep
