fe041c784c67cd592ff8f5468fb7335cbe68f0fe
jobaer6768 <jobaer9898@gmail.com>
2025-02-04 20:15:24 +0600

    added divider in FAQ component

--- src/components/faq/Index.jsx
+++ src/components/faq/Index.jsx
@@ -1,4 +1,5 @@
 import Accordion from "../ui/Accordion";
+import Divider from "../ui/Divider";
 
 const FAQ = () => {
   return (
@@ -17,21 +18,25 @@ const FAQ = () => {
         content="Concept is an idea wrapped into a design. Depending on the complexity of the product, it’s either a 3-5 informational blocks (landing page), a 2-5 of screens (mobile app), or a 2-primary screens (dashboard). If, to be recognized the idea requires animation — it can be added free of charge."
         defaultOpen={true}
       />
+      <Divider className="my-10" />
 
       <Accordion
         title="What is a concept, and what does it include?"
         content="Concept is an idea wrapped into a design. Depending on the complexity of the product, it’s either a 3-5 informational blocks (landing page), a 2-5 of screens (mobile app), or a 2-primary screens (dashboard). If, to be recognized the idea requires animation — it can be added free of charge."
       />
+      <Divider className="my-10" />
 
       <Accordion
         title="What is a concept, and what does it include?"
         content="Concept is an idea wrapped into a design. Depending on the complexity of the product, it’s either a 3-5 informational blocks (landing page), a 2-5 of screens (mobile app), or a 2-primary screens (dashboard). If, to be recognized the idea requires animation — it can be added free of charge."
       />
+      <Divider className="my-10" />
 
       <Accordion
         title="What is a concept, and what does it include?"
         content="Concept is an idea wrapped into a design. Depending on the complexity of the product, it’s either a 3-5 informational blocks (landing page), a 2-5 of screens (mobile app), or a 2-primary screens (dashboard). If, to be recognized the idea requires animation — it can be added free of charge."
       />
+      <Divider className="my-10" />
     </div>
   );
 };
---
+++ src/components/ui/Divider.jsx
@@ -0,0 +1,5 @@
+const Divider = () => {
+  return <div className="w-[1548px] border border-[#E1E1E1]"></div>;
+};
+
+export default Divider;
