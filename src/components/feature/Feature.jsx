e2bb968394558b2d60ffd2e6db5767bcb56027c4
HIMU005 <hashanuzzaman99@gmail.com>
2025-02-04 18:08:32 +0600

    Make a different component

---
+++ src/components/feature/Feature.jsx
@@ -0,0 +1,28 @@
+import Card from "../ui/Card";
+import Title from "../ui/Title";
+
+const Feature = () => {
+  return (
+    <div className="w-[1920px] ">
+      <Title
+        title1={"OUR PORTFOLIO"}
+        title2={
+          "User-centered design encourages productivity and boosts revenue"
+        }
+      />
+      <div className="px-[160px] mt-[84px]">
+        {[1, 3, 5].map((id) => (
+          <div
+            key={id}
+            className="h-[817px] mb-[48px] flex justify-between gap-[46px]"
+          >
+            <Card id={id} />
+            <Card id={id + 1} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Feature;
