c1cbc690dcc1e239b3c8d85555b9b5379d1fb742
HIMU005 <hashanuzzaman99@gmail.com>
2025-02-06 11:26:40 +0600

    finished the task

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import Step from "./Step";
+import profileImg from "./assets/profile.svg";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="w-[382px] md:w-[660px] lg:w-[942px] mx-auto mt-[132px] md:mt-[189px] lg:mt-[144px]">
+      <Step />
+      {/* input field and button */}
+      <div className="mt-[173px] md:mt-[215px] lg:mt-[244px] mx-auto w-[382px] md:w-[452px] lg:w-[480px]">
+        <img className="mx-auto" src={profileImg} alt="" />
+        <input
+          className="rounded-[94px] flex justify-center items-center py-2 px-4 placeholder:text-[#939393] border border-[#D2D2D2] mx-auto placeholder:text-center my-3"
+          type="file"
+          placeholder="Upload profile picture"
+        ></input>
+        <button className="rounded-[100px] w-full h-[56px] bg-gradient-to-r from-[#FFB800] to-[#FF7A00] text-white cursor-pointer">
+          Send OTP
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Step.jsx
@@ -0,0 +1,35 @@
+const Step = () => {
+  const steps = [1, 2, 3, 4, 5, 6];
+  const currentStep = 6; // Example: Step 3 is active
+  return (
+    <div className=" md:w-[660px] lg:w-[942px] flex items-center justify-center space-x-0 p-4 rounded-md">
+      {steps.map((step, index) => (
+        <div key={step} className="flex items-center gap-0 text-sm">
+          {/* Line before each step except the first */}
+          {index !== 0 && (
+            <div
+              className={`h-1 w-[47.6px] md:w-[95.8px] lg:w-[150px] ${
+                step <= currentStep ? "bg-orange-500" : "bg-gray-300"
+              }`}
+            ></div>
+          )}
+
+          {/* Step circle */}
+          <div
+            className={`w-6 md:w-[30.16px] h-6 md:h-[30.16px] flex items-center justify-center rounded-full border ${
+              step < currentStep
+                ? " bg-gradient-to-r from-[#FFB800] to-[#FF7A00] text-white border-none"
+                : step === currentStep
+                ? "border border-[#FFB800]  text-orange-500 bg-white"
+                : "border-gray-300 text-black"
+            }`}
+          >
+            {step}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Step;
