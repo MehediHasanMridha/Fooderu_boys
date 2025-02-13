0929845c8f0e8c39a9ab4de0c7acbe6dc0e7077e
jobaer6768 <jobaer9898@gmail.com>
2025-02-13 21:58:40 +0600

    fixed major bug and made responsive for phone, tablet and desktop devices

--- src/stories/Index.jsx
+++ src/stories/Index.jsx
@@ -1,15 +1,15 @@
 import StoryCard from "../storyCard/Index";
 
-import firstStory from "../assets/first_story.jpg";
-import secondStory from "../assets/second_story.jpg";
-import thirdStory from "../assets/third_story.jpg";
+import storyPic from "../assets/storyPic.jpg";
 
 const Stories = () => {
   return (
-    <div className="my-4 ml-2 flex items-center gap-2">
-      <StoryCard img={firstStory} />
-      <StoryCard img={secondStory} />
-      <StoryCard img={thirdStory} />
+    <div className="my-4 ml-2 flex items-center justify-center gap-2 md:my-8 md:ml-4 md:gap-3 2xl:ml-3">
+      <StoryCard img={storyPic} />
+      <StoryCard img={storyPic} />
+      <StoryCard img={storyPic} />
+      <StoryCard img={storyPic} />
+      <StoryCard img={storyPic} />
     </div>
   );
 };
--- src/storyCard/Index.jsx
+++ src/storyCard/Index.jsx
@@ -2,12 +2,22 @@
 const StoryCard = ({ img }) => {
   return (
     <div
-      className="w-[128px] h-[173.18px] rounded-[8px] bg-cover bg-center"
+      className="relative w-[128px] h-[173.18px] rounded-[8px] bg-cover bg-center bg-no-repeat flex flex-col justify-end p-2 2xl:w-[170px] 2xl:h-[230px]"
       style={{
         backgroundImage: `url(${img})`,
-        backgroundColor: "#D9D9D9",
       }}
-    ></div>
+    >
+      {/* Dark Gradient Overlay at the Bottom */}
+      <div className="absolute inset-0 bg-gradient-to-t from-black/50 to-transparent rounded-[8px]"></div>
+
+      {/* Text element */}
+      <div className="relative z-10 text-white font-inter">
+        <p className="truncate text-[14px] 2xl:text-[18px] 2xl:font-medium 2xl:leading-[30px]">
+          Special salad vegetable
+        </p>
+        <p className="truncate text-[12px]">Blue Chines House</p>
+      </div>
+    </div>
   );
 };
 
