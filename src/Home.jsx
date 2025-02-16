382336a7e661f64be2bbc113f5855bde5b36836a
HIMU005 <hashanuzzaman99@gmail.com>
2025-02-16 22:02:37 +0600

    ratting and comment part done

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,14 @@
 import { BsThreeDots } from "react-icons/bs";
+import { FaCommentDots, FaLocationDot, FaStar } from "react-icons/fa6";
+import { GiFlamer } from "react-icons/gi";
+import foodImage from "./assets/foodItem.png";
 import profileImage from "./assets/restaurantProfile.png";
 
 const Home = () => {
   return (
     <div className="w-[430px] border border-red-500 p-0.5">
       {/* upper part  */}
+
       <div className="flex justify-between gap-4">
         {/* profile part  */}
         <div className="flex justify-start gap-2">
@@ -25,6 +29,32 @@ const Home = () => {
           <BsThreeDots />
         </div>
       </div>
+      <h2 className="text-[#161616] text-sm leading-[22px] py-2 ">
+        Chicken biryani is a quintessential South Asian dish known for its rich
+        flavors and aromatic spices.
+      </h2>
+      {/* image part  */}
+      <img className="w-full items-stretch h-[246px] " src={foodImage} alt="" />
+      <div className="flex justify-between ">
+        <div className="flex justify-start mt-1.5 ">
+          <FaStar className="text-[#FFB800] mr-0.5 " />
+          <p className="text-[#585858] text-xs ">
+            4.5 Out of 50 members{" "}
+            <span className="text-[#D2D2D2] mx-1 ">|</span>
+          </p>
+          <FaCommentDots className="text-[#2131F0] mr-0.5" />
+          <p className="text-[#585858] text-xs ">
+            14 <span className="text-[#D2D2D2] mx-1 ">|</span>
+          </p>
+          <FaLocationDot className="text-[#9F7CEC] mr-0.5 " />
+          <p className="text-[#585858] text-xs ">Tangail</p>
+        </div>
+        <div className="flex gap-0.5 md:hidden">
+          <GiFlamer className="text-[#EC8154] mr-0.5" />
+          <p className="text-[#585858] text-xs ">Trending now</p>
+        </div>
+      </div>
+      {/* rating here  */}
     </div>
   );
 };
