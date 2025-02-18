9884fd9f859a3a66b37f5ad2ad7c4573e1bd1e1c
HIMU005 <hashanuzzaman99@gmail.com>
2025-02-18 23:43:39 +0600

    change the breakpoint

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -14,47 +14,47 @@ import logo from "./assets/Logo (2).png";
 
 const Navbar = () => {
   return (
-    <div className="w-[430px] md:w-[771px] lg:w-[1772px] mx-auto md:px-1 md:py-2 md:relative lg:h-16">
+    <div className="w-[430px] md:w-[771px] xl:w-[1772px] mx-auto md:px-1 md:py-2 md:relative xl:h-16">
       {/* top line  */}
       <div className="flex justify-between mt-4 ">
         <div className="flex justify-start gap-1.5">
-          <IoReorderThreeOutline className="text-2xl lg:hidden" />
+          <IoReorderThreeOutline className="text-2xl xl:hidden" />
           <img
-            className="w-[100px] h-[30px] md:w-[80px] lg:w-[181px] lg:h-14 "
+            className="w-[100px] h-[30px] md:w-[80px] xl:w-[181px] xl:h-14 "
             src={logo}
             alt=""
           />
         </div>
-        <div className="flex justify-start gap-1.5 text-2xl relative lg:w-[412px] ">
+        <div className="flex justify-start gap-1.5 text-2xl relative xl:w-[412px] ">
           {/* search icon  */}
-          <CiSearch className="lg:absolute lg:left-3 lg:top-3 " />
+          <CiSearch className="xl:absolute xl:left-3 xl:top-3 " />
           <input
-            className="hidden lg:block placeholder:text-[#939393] placeholder:text-lg px-4 rounded-[56px] pl-16 border border-[#D2D2D2] "
+            className="hidden xl:block placeholder:text-[#939393] placeholder:text-lg px-4 rounded-[56px] pl-16 border border-[#D2D2D2] "
             type="text"
             name=""
             id=""
             placeholder="search"
           />
-          <IoIosNotificationsOutline className="lg:text-[32px] text-[#161616] lg:mt-3 " />
+          <IoIosNotificationsOutline className="xl:text-[32px] text-[#161616] xl:mt-3 " />
         </div>
       </div>
 
       {/* navigation button  */}
-      <div className="flex justify-between mt-2 gap-1  md:absolute md:left-1/2 md:-translate-x-1/2 md:w-[530px]  top-2 lg:top-4 lg:mr-3 ">
+      <div className="flex justify-between mt-2 gap-1  md:absolute md:left-1/2 md:-translate-x-1/2 md:w-[530px]  top-2 xl:top-4 xl:mr-3 ">
         {/* news button  */}
-        <button className="flex items-center justify-center mx-auto bg-[#F6F6F6] w-[80px] h-12 md:w-[65px] lg:w-[165px] rounded-[8px] pyh-12 d:py-0 border-b-2 lg:border-0 border-[#2131F0] md:flex-col md:justify-start md:h-12 ">
+        <button className="flex items-center justify-center mx-auto bg-[#F6F6F6] w-[80px] h-12 md:w-[65px] xl:w-[165px] rounded-[8px] pyh-12 d:py-0 border-b-2 xl:border-0 border-[#2131F0] md:flex-col md:justify-start md:h-12 ">
           <IoNewspaperOutline className="rounded-[86.667px] bg-gradient-to-br from-[#2131F0] to-[#0A82FE] text-lg text-white p-1 mt-1 " />
           <h2 className="hidden md:block text-base text-[#161616] ">Feed</h2>
         </button>
 
         {/* trading button  */}
-        <button className="flex items-center justify-center mx-auto w-[80px] h-12 md:w-[65px] lg:w-[165px] rounded-2xl py-2h-12 py-0 md:flex-col md:h-12  ">
+        <button className="flex items-center justify-center mx-auto w-[80px] h-12 md:w-[65px] xl:w-[165px] rounded-2xl py-2h-12 py-0 md:flex-col md:h-12  ">
           <IoFlameOutline className="text-2xl md:text-lg" />
           <h2 className="hidden md:block text-base text-[#585858] ">Trading</h2>
         </button>
 
         {/*  restaurant button */}
-        <button className="flex items-center justify-center mx-auto w-[80px] h-12 md:w-[65px] lg:w-[165px] py-2 md:py-0 md:h-12  md:flex-col">
+        <button className="flex items-center justify-center mx-auto w-[80px] h-12 md:w-[65px] xl:w-[165px] py-2 md:py-0 md:h-12  md:flex-col">
           <AiOutlineShop className="text-2xl md:text-lg" />
           <h2 className="hidden md:block text-base text-[#585858] ">
             Restaurant
@@ -62,13 +62,13 @@ const Navbar = () => {
         </button>
 
         {/* event button  */}
-        <button className="flex items-center justify-center mx-auto w-[80px] h-12 md:w-[65px] lg:w-[165px] py-2 md:py-0 md:h-12  md:flex-col">
+        <button className="flex items-center justify-center mx-auto w-[80px] h-12 md:w-[65px] xl:w-[165px] py-2 md:py-0 md:h-12  md:flex-col">
           <TbSpeakerphone className="text-2xl md:text-lg" />
           <h2 className="hidden md:block text-base text-[#585858] ">Event</h2>
         </button>
 
         {/* Profile button  */}
-        <button className="flex items-center justify-center mx-auto w-[80px] h-12 md:w-[65px] lg:w-[165px] py-2 md:py-0 md:h-12  md:flex-col">
+        <button className="flex items-center justify-center mx-auto w-[80px] h-12 md:w-[65px] xl:w-[165px] py-2 md:py-0 md:h-12  md:flex-col">
           <HiOutlineUser className="text-2xl md:text-lg" />
           <h2 className="hidden md:block text-base text-[#585858] ">Profile</h2>
         </button>
