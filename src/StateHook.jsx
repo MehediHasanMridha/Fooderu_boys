6cad350856d321106204951ed8513acd2dee4e5f
jobaer6768 <jobaer9898@gmail.com>
2025-02-19 23:04:12 +0600

    polishing

---
+++ src/StateHook.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import { flushSync } from "react-dom";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+
+let root;
+
+let render = 0;
+const states = {};
+
+const useState = (value) => {
+  const id = render++;
+
+  if (states[id]) return states[id];
+
+  const setValue = (newValue) => {
+    states[id][0] = newValue;
+    manualRerender();
+  };
+
+  const state = [value, setValue];
+  states[id] = state;
+
+  return state;
+};
+
+const StateHook = () => {
+  const [count, setCount] = useState(1);
+  const [error, setError] = useState(null);
+
+  console.log("Count", count);
+  console.log("Error", error);
+
+  const adder = () => {
+    if (count < 10) {
+      setCount(count + 1);
+      setError(null);
+    } else {
+      setError("You cant go higher than 10");
+    }
+  };
+
+  const subtractor = () => {
+    if (count > 1) {
+      setCount(count - 1);
+      setError(null);
+    } else {
+      setError("You cant go lower than 1");
+    }
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center w-full h-screen">
+      <div className="space-x-4 text-2xl">
+        <button
+          className="border border-amber-600 p-4 cursor-pointer"
+          onClick={subtractor}
+        >
+          -
+        </button>
+        <span className="font-bold">{count}</span>
+        <button
+          className="border border-amber-600 p-4 cursor-pointer"
+          onClick={adder}
+        >
+          +
+        </button>
+      </div>
+      <p className="my-2 text-red-500 text-xl font-medium">{error}</p>
+    </div>
+  );
+};
+
+const manualRerender = () => {
+  render = 0;
+
+  flushSync(() => {
+    root.render(<StateHook />);
+  });
+};
+
+const rootElement = document.getElementById("root");
+root = ReactDOM.createRoot(rootElement);
+
+manualRerender();
+
+export default StateHook;
