56278717f95ab0b1d7c7368a79ab2278dbcd9eee
Anamul029 <anamhaque.5101@gmail.com>
2025-02-19 22:40:41 +0600

    update design

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import GeneralPost from "./components/GeneralPost";
 import Navbar from "./components/NavBar";
+import Stories from "./components/Stories/Index";
 
 
 const App = () => {
   return (
     <>
        <Navbar></Navbar>
+       <Stories></Stories>
         <GeneralPost></GeneralPost>
     </>
   );
